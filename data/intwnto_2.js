c405d632d910b3940d6d1b1ca9aa63f605b8b3c2
gabriel9recursos <85639382+gabriel9recursos@users.noreply.github.com>
2021-06-09 14:55:04 -0400

    Initial commit

---
+++ data/intwnto_2.js
@@ -0,0 +1,15 @@
+var json_intwnto_2 = {
+"type": "FeatureCollection",
+"name": "intwnto_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "valor": "10", "usuario": "doña treme" }, "geometry": { "type": "Point", "coordinates": [ -70.928627536155503, -28.5070474705295 ] } },
+{ "type": "Feature", "properties": { "valor": "40", "usuario": "coné" }, "geometry": { "type": "Point", "coordinates": [ -70.947720287446415, -28.549244055315313 ] } },
+{ "type": "Feature", "properties": { "valor": "500", "usuario": "chespirito" }, "geometry": { "type": "Point", "coordinates": [ -70.893811342625, -28.555180456977194 ] } },
+{ "type": "Feature", "properties": { "valor": "1000", "usuario": "don ramon" }, "geometry": { "type": "Point", "coordinates": [ -70.882580312453868, -28.51795761412431 ] } },
+{ "type": "Feature", "properties": { "valor": "35", "usuario": "dr house" }, "geometry": { "type": "Point", "coordinates": [ -70.374052095337674, -30.611696063566669 ] } },
+{ "type": "Feature", "properties": { "valor": "500", "usuario": "greys" }, "geometry": { "type": "Point", "coordinates": [ -71.28477835755119, -31.49325315723955 ] } },
+{ "type": "Feature", "properties": { "valor": "900", "usuario": "romané" }, "geometry": { "type": "Point", "coordinates": [ -70.43239043241897, -32.62112767414456 ] } },
+{ "type": "Feature", "properties": { "valor": "58", "usuario": "lazlo" }, "geometry": { "type": "Point", "coordinates": [ -71.505167630969424, -34.56249789146095 ] } }
+]
+}
